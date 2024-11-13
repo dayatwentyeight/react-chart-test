6bcfa85a6c630378e3e292352f8c525e573a4306
dayatwentyeight <dayafromkr@gmail.com>
2024-11-13 14:27:50 +0900

    feat: add browser routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import "./index.css";
+import App from "./App.tsx";
+import Layout from "./Layout.tsx";
+import RechartVsChartJSPage from "./pages/RechartVsChartJSPage.tsx";
 
-createRoot(document.getElementById('root')!).render(
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/rechart-vs-chartjs",
+        element: <RechartVsChartJSPage />,
+      },
+      {
+        path: "/network-chart",
+        element: <div></div>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  </StrictMode>
+);
